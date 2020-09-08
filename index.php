6fc064768c5818bed44e9866bc1bc5bd0973ca53
membreno <cmembreno048@gmail.com>
2020-09-07 21:26:29 -0600

    agregamos el sistema de impresiones :v

--- index.php
+++ index.php
@@ -10,20 +10,20 @@
 
     <title>Ingreso tickets</title>
   </head>
-  <body style="background-color: #E1E6E3;">
+  <body style="background-color: #F6FBF8;">
 
   <!--  comienzo del navbar-->
 
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light ">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <a class="navbar-brand" href="#">Lacteos Pueblo Viejo</a>
+  <a class="navbar-brand" href="index.php">Lacteos Pueblo Viejo</a>
 
   <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
       <li class="nav-item active">
-        <a class="nav-link" href="#"> Clientes </a>
+        <a class="nav-link" href="pages/pedidos.php"> Clientes </a>
       </li>
       <li class="nav-item active">
         <a class="nav-link" href="#"></a>
@@ -35,10 +35,44 @@
 
 <!--  fin del navbar-->
 
+<!--  Cminezo tabla-->
+
+<div class="container" >
+
+  <div class="table-responsive">
+  <table class="table table-striped table-bordered table-hover">
+<caption>Pedidos realizados</caption>
+<thead>
+<tr>
+   <th>Codido de pedido</th>
+   <th>Cliente</th>
+   <th>Total</th>
+   <th>Opciones</th>
+ </tr>
+</thead>
+
+<tbody>
+<tr>
+   <td>1</td>
+   <td>Cristian Membreño</td>
+   <td>200</td>
+   <td><button></button></td>
+ </tr>
+ 
+</tbody>
+
+</table>
+  </div>
+
+</div>
+
+<!--  fin tabla-->
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
   </body>
+
 </html>
